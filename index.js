c1e39546bad80baf1acca5045d7380865668d552
Cancela <2212509@my.ipleiria.pt>
2023-11-25 17:47:28 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { randomBytes } = require("crypto");
+
+const httpServer = require("http").createServer();
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+httpServer.listen(8081, () => {
+  console.log("listening on *:8081");
+});
+
+io.on("connection", (socket) => {
+  console.log(`client ${socket.id} has connected`);
+
+  socket.on("echo", (message) => {
+    socket.emit("echo", message);
+  });
+});
